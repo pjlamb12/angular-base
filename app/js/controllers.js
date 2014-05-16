93f1f850a0c9cb79754c44e0afd125bbfd28985e
Preston Lamb <preston.j.lamb@gmail.com>
2014-05-15 23:42:50 -0600

    First commit, with angular-seed as the majority, with a few other changes.

---
+++ app/js/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('HomeCtrl', ['$scope', function($scope) {
+
+  }])
+  .controller('MyCtrl1', ['$scope', function($scope) {
+
+  }])
+  .controller('MyCtrl2', ['$scope', function($scope) {
+
+  }]);
