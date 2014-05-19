32f413e7a5ef144872bf880e4f76e79fd860174f
Preston Lamb <preston.j.lamb@gmail.com>
2014-05-19 11:07:24 -0600

    Switched routing to angular-ui-router

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 var myApp = angular.module('myApp', [
   'ngRoute',
+  'ui.router',
   'myApp.view1'
   // 'myApp.filters',
   // 'myApp.services',
